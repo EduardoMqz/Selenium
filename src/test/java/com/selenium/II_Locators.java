bb2e33fc815158f46182251fe7d2f4d0868d6878
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2025-04-16 17:08:04 -0600

    add dependecies

--- src/test/java/com/selenium/II_Locators.java
+++ src/test/java/com/selenium/II_Locators.java
@@ -1,12 +1,26 @@
 package com.selenium;
 
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class II_Locators {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "path to chromedriver");
-        Webdriver driver = new ChromeDriver();
-        driver.get("webpage");
 
+    
+    
+  
+    
+
+    @Test
+    public void logInTest() {
+        System.setProperty("webdriver.chrome.driver", "path to chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("google.com");
         driver.findElement(By.id("elementsId")).sendKeys("text2Type");
         driver.findElement(By.name("elementName")).sendKeys("text");
         driver.findElement(By.className("className")).click();
