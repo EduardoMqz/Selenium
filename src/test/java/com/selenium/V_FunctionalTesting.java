fd416c4989a46cb2008992565dca1283fb394048
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2025-05-09 21:05:45 -0600

    finish test

--- src/test/java/com/selenium/V_FunctionalTesting.java
+++ src/test/java/com/selenium/V_FunctionalTesting.java
@@ -5,20 +5,19 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import com.resources.JsonReader;
 
 public class V_FunctionalTesting {
 
     @Test
     public void cart() throws IOException{
+        String[] productsToAdd = {"Cucumber", "Brinjal", "Mushroom", "Corn","Mango", "Pomegranate", "Almonds", "Walnuts"};
         Map<String, Object> config = JsonReader.readJsonAsMap("src/test/java/com/resources/V_FunctionalTesting.json");
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
@@ -26,16 +25,14 @@ public class V_FunctionalTesting {
 
         driver.get((String) config.get("webPage"));
         List<WebElement> products = driver.findElements(By.className("product"));
-        String[] productList = {"Cucumber", "Brinjal", "Mushroom", "Corn"};
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).findElement(By.className("product-name")).getText().contains("Cucumber")) {
+        for (int i = 0; i < products.size()-1; i++) {
+           String productName = products.get(i).findElement(By.cssSelector("[class*='product-name']")).getText();
+            if (Arrays.asList(productsToAdd).contains(productName.split(" ")[0])) {
                 while (!products.get(i).findElement(By.className("stepper-input")).findElement(By.className("quantity")).getDomAttribute("value").equalsIgnoreCase("5")) {
                     products.get(i).findElement(By.className("stepper-input")).findElement(By.className("increment")). click();
                 }
-                products.get(i).findElement(By.className("product-action")).click();
-                break;  
+                products.get(i).findElement(By.className("product-action")).click(); 
             }
-            
         }
        driver.quit();   
     }
