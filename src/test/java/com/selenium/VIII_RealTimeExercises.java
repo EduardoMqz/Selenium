909bf23c48b109b9bce74697bf86a7a78c298cd7
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2025-05-27 21:20:42 -0600

    open multiple links

--- src/test/java/com/selenium/VIII_RealTimeExercises.java
+++ src/test/java/com/selenium/VIII_RealTimeExercises.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,7 +16,7 @@ public class VIII_RealTimeExercises {
     private static final String JSON_PATH = "src\\test\\java\\com\\resources\\VIII_RealTimeExercises.json";
 
     @Test
-    public void linksOnPage() throws IOException{
+    public void linksOnPage() throws IOException {
         Map<String, Object> config = JsonReader.readJsonAsMap(JSON_PATH);
         WebDriver driver = new ChromeDriver();
 
@@ -23,9 +24,20 @@ public class VIII_RealTimeExercises {
         driver.manage().window().maximize();
         List<WebElement> listOfLinks = driver.findElements(By.xpath("//a"));
         System.out.println(listOfLinks.size());
-        WebElement footerDriver = driver.findElement(By.id("gf-BIG"));
+        WebElement footerDriver = driver.findElement(By.id("gf-BIG")); // limiting webdriver scope
         List<WebElement> listOfFooterLInks = footerDriver.findElements(By.tagName("a"));
         System.out.println(listOfFooterLInks.size());
+        // count of only first column
+        WebElement columnOneDriver = footerDriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
+        List<WebElement> listOfLinksColumnOne = columnOneDriver.findElements(By.tagName("a"));
+        System.out.println(listOfLinksColumnOne.size());
+        // click on each link and check page if the page are opening
+        listOfLinksColumnOne.stream()
+                .skip(1) // Skip the first element (equivalent to starting at i=1)
+                .forEach(link -> {
+                    String cclickOnLink = Keys.chord(Keys.CONTROL, Keys.ENTER);
+                    link.sendKeys(cclickOnLink);
+        });
         driver.quit();
     }
 
