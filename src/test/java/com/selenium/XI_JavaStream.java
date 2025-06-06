7b36b03722e7e728015b37f091e7b4a802cfb228
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2025-06-06 13:59:00 -0600

    streams filter, map, sorted

--- src/test/java/com/selenium/XI_JavaStream.java
+++ src/test/java/com/selenium/XI_JavaStream.java
@@ -1,6 +1,7 @@
 package com.selenium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -34,11 +35,22 @@ public class XI_JavaStream {
         long countStream = names.stream().filter(name -> name.startsWith("A")).count();
         System.out.println("using stream: " + countStream);
 
-        countStream = Stream.of("David", "Allen", "George", "Adam", "Emma", "Mary").filter(name -> {
-            name.startsWith("A");
-            return false;
-        }).count();
+        countStream = Stream.of("David", "Allen", "George", "Adam", "Emma", "Mary").filter(name -> name.startsWith("A"))
+            .count();
         System.out.println("using stream directly: " + countStream);
+
+        names.stream().filter(name -> name.length() > 4)
+            .forEach(name -> System.out.println(name + " is greater than 4"));
+
+        names.stream().filter(name -> name.length() > 4).limit(1)
+            .forEach(name -> System.out.println(name + " is alone"));
+
+        Stream.of("sophia", "liam", "olivia", "noah", "ava", "ethan").filter(name -> name.endsWith("a"))
+            .map(name -> name.toUpperCase()).forEach(name -> System.out.println(name + " ends with a"));
+
+        List<String> nom = Arrays.asList("oliva", "sophia", "owen", "ethan", "oliver");
+        nom.stream().filter(name -> name.startsWith("o"))
+            .map(name -> name.toUpperCase()).sorted().forEach(name -> System.out.println(name + " starts with an O"));
         /****************************************************/
     }
 }
