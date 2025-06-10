ffea5b0bf669d0002ebb1506dbc5901287625fff
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2025-06-10 13:59:27 -0600

    validate sorted web table

--- src/test/java/com/selenium/XI_JavaStream.java
+++ src/test/java/com/selenium/XI_JavaStream.java
@@ -1,17 +1,27 @@
 package com.selenium;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.google.common.collect.Streams;
+import com.resources.JsonReader;
 
 public class XI_JavaStream {
+
+    private static final String JSON_PATH = "src\\test\\java\\com\\resources\\XI_JavaStream.json";
 
     @Test
     public void javaStreams() {
@@ -72,5 +82,20 @@ public class XI_JavaStream {
         List<Integer> numb = numbers.stream().distinct().sorted().collect(Collectors.toList());
         System.out.println("number in index 2: " + numb.get(2));
         /****************************************************/
+    }
+
+    @Test
+    public void webTableSorting() throws IOException{
+        Map<String,Object> config = JsonReader.readJsonAsMap(JSON_PATH);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(config.get("webPage").toString());
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//th[1]")).click();
+        List<WebElement> elementsList = driver.findElements(By.xpath("//tbody/tr/td[1]"));
+        List<String> productsList = elementsList.stream().map(product -> product.getText()).collect(Collectors.toList());
+        List<String> sortedList = productsList.stream().sorted().collect(Collectors.toList());
+        Assert.assertEquals(productsList, sortedList);
+        driver.quit();
     }
 }
