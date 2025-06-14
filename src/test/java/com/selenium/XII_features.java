cc8d14e60296ae9dbfadd9ebf8f37363e283714b
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2025-06-13 21:38:30 -0600

    switch between tabs

--- src/test/java/com/selenium/XII_features.java
+++ src/test/java/com/selenium/XII_features.java
@@ -2,12 +2,12 @@ package com.selenium;
 
 import java.io.IOException;
 import java.util.Map;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import com.resources.JsonReader;
 import static org.openqa.selenium.support.locators.RelativeLocator.*;
@@ -25,6 +25,22 @@ public class XII_features {
         WebElement nameTxt = driver.findElement(By.cssSelector("[name='name']"));
         String labelTxt = driver.findElement(with(By.tagName("label")).above(nameTxt)).getText();
         Assert.assertTrue(labelTxt.equalsIgnoreCase("name"));
+        driver.quit();
+    }
+
+    @Test
+    public void multipleTabs() throws IOException{
+        Map<String,Object> config = JsonReader.readJsonAsMap(JSON_PATH);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(config.get("webPage").toString());
+        driver.manage().window().maximize();
+        String parentWindow = driver.getWindowHandle();
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get(config.get("webPageCourses").toString());
+        String courseTtitle = driver.findElements(By.cssSelector("a[href*='/p/'] div[class='course-listing-title']")).get(1).getText();
+        driver.switchTo().window(parentWindow);
+        driver.findElement(By.cssSelector("[name='name']")).sendKeys(courseTtitle);
         driver.quit();
     }
 }
