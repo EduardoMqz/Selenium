e9ed4d5f7477097a8a0c12c42dd55bc3a0f51276
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2025-06-17 12:40:06 -0600

    testng xml

--- src/test/java/com/selenium/XIII_Framework_TestNG.java
+++ src/test/java/com/selenium/XIII_Framework_TestNG.java
@@ -1,13 +1,19 @@
 package com.selenium;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 public class XIII_Framework_TestNG {
     private static final String JSON_PATH = "src\\test\\java\\com\\resources\\XIII_Framework_TestNG.json";
 
     @Test
-    public void testNrfG(){
+    public void testNG(){
+        System.out.println("Hello world");
 
+    }
+
+    @Test
+    public void secondTest(){
+        System.out.println("second Test");
     }
 
 }
